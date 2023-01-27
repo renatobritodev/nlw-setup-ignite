afade4e62becf85cc750554abaf2663743398f72
Renato Brito <dev.renatobrito@gmail.com>
2023-01-26 23:24:37 -0300

    :construction: New updates

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -45,9 +45,10 @@ export function Home() {
             amountOfDaysToFill > 0 && Array
             .from({ length: amountOfDaysToFill })
             .map((_, i) => (
-              <View 
-              className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-              style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              <View
+                key={i}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
               ))
             }
