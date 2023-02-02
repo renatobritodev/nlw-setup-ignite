bc36639f85fab7a6566973a7104e8987c5af74c0
Renato Brito <dev.renatobrito@gmail.com>
2023-02-02 19:09:34 -0300

    :tada: File renamed from Routers to Routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { appRouters } from './routers'
+import { appRoutes } from './routes'
 
 const app = Fastify()
 
 app.register(cors)
-app.register(appRouters)
+app.register(appRoutes)
 
 app.listen({
 	port: 3333
