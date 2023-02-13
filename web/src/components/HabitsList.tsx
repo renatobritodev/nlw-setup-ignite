7624c4192d3a4bbcf56c6e0e00fe94974ca631ec
Renato Brito <dev.renatobrito@gmail.com>
2023-02-12 21:51:31 -0300

    :tada: Components finished

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -4,7 +4,7 @@ import { Check } from 'phosphor-react'
 import { useEffect, useState } from 'react'
 import { api } from '../lib/axios'
 
-interface HabitsListProps {
+interface HabitLisProps {
   date: Date
   onCompletedChanged: (completed: number) => void
 }
@@ -14,36 +14,37 @@ interface HabitsInfo {
     id: string
     title: string
     created_at: string
-  }[],
+  }[]
   completedHabits: string[]
 }
 
-export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
+export function HabitsList({ date, onCompletedChanged }: HabitLisProps) {
+
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
 
   useEffect(() => {
     api.get('day', {
-      params: {
+      params:{
         date: date.toISOString()
       }
     }).then(response => {
       setHabitsInfo(response.data)
     })
-  }, [])
+  },[])
 
   async function handleToggleHabit(habitId: string) {
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+  
     await api.patch(`/habits/${habitId}/toggle`)
-    
-    const isHabitAlreadyCompleted = habitsInfo?.completedHabits.includes(habitId)
 
     let completedHabits: string[] = []
 
-    if (isHabitAlreadyCompleted) {
+    if(isHabitAlreadyCompleted) {
       completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
     } else {
       completedHabits = [...habitsInfo!.completedHabits, habitId]
     }
-    
+
     setHabitsInfo({
       possibleHabits: habitsInfo!.possibleHabits,
       completedHabits
@@ -52,34 +53,32 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
     onCompletedChanged(completedHabits.length)
   }
 
-  const isDayInPast = dayjs(date)
-    .endOf('day')
-    .isBefore(new Date())
+  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
 
   return (
-    <div className="mt-6 flex flex-col gap-3">
-      {habitsInfo!.possibleHabits.map(habit => {
+    <div className='mt-6 flex flex-col gap-3'>
+      {habitsInfo?.possibleHabits.map(habit => {
         return (
           <Checkbox.Root
             key={habit.id}
-            onCheckedChange={() => {}}
-            checked={habitsInfo?.completedHabits.includes(habit.id)}
-            disabled={isDayInPast}
-            className="flex items-center gap-3 group"
+            onCheckedChange={() => handleToggleHabit(habit.id)}
+            checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPast}
+            className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
           >
-            <div
-              className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
             </div>
 
-            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+            <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
               {habit.title}
             </span>
           </Checkbox.Root>
         )
       })}
+      
     </div>
   )
 }
--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -6,11 +6,13 @@ export function ProgressBar(props: ProgressBarProps) {
   return (
     <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
       <div 
-        role='progressbar'
-        aria-label='Progresso de hábitos completados nesse dia'
-        aria-aria-valuenow={props.progress}
-        className='h-3 rounded-xl bg-violet-600 transition-all'
-        style={{width: `${props.progress}%`}}
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600 transition-all"
+        style={{
+          width: `${props.progress}%`
+        }}
       />
     </div>
   )
