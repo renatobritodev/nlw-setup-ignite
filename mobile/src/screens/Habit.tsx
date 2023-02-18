c736023815d71a51d5662a3c32689b15f383e155
Renato Brito <dev.renatobrito@gmail.com>
2023-02-18 00:28:19 -0300

    :tada: screens updated

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,22 +1,88 @@
-import { View, ScrollView, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import { View, ScrollView, Text, Alert } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import dayjs from 'dayjs'
+import clsx from 'clsx'
+
+import { api } from '../lib/axios'
+import { generateProgressPercentage } from '../utils/generate-progress-percentage'
 
 import { BackButton } from '../components/BackButton'
 import { ProgressBar } from '../components/ProgressBar'
 import { Checkbox } from '../components/Checkbox'
+import { Loading } from '../components/Loading'
+import { HabitsEmpty } from '../components/HabitsEmpty'
 
 interface Params {
   date: string
 }
 
+interface DayInfoProps {
+  completedHabits: string[]
+  possibleHabits: {
+    id: string
+    title: string
+  }[]
+}
+
 export function Habit() {
+  const [loading, setLoading] = useState(true)
+  const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null)
+  const [completedHabits, setCompletedHabits] = useState<string[]>([])
+
   const route = useRoute()
   const { date } = route.params as Params
 
   const parsedDate = dayjs(date)
+  const isDateInPast = parsedDate.endOf('day').isBefore(new Date())
   const dayOfWeek = parsedDate.format('dddd')
   const dayAndMonth = parsedDate.format('DD/MM')
+
+  const habitProgress = dayInfo?.possibleHabits.length 
+    ? generateProgressPercentage(dayInfo.possibleHabits.length, completedHabits.length) 
+    :0
+
+  async function fetchHabits() {
+    try {
+      setLoading(true)
+
+      const response = await api.get('day', { params: { date } })
+      setDayInfo(response.data)
+      setCompletedHabits(response.data.completedHabits)
+
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Oops! Não foi possível carregar as informações dos hábitos.')
+    }
+    finally {
+      setLoading(false)
+    }
+  }
+
+  async function handToggleHabit(habitId: string) {
+    try {
+      await api.patch(`/habits/${habitId}/toggle`)
+      
+      if(completedHabits.includes(habitId)) {
+        setCompletedHabits(prevState => prevState.filter(habit => habit !== habitId))
+      } else {
+        setCompletedHabits(prevState => [...prevState, habitId])
+      }
+    } catch(error) {
+      console.log(error)
+      Alert.alert('Oops!, Não foi possível atualizar o status do hábito.')
+    }
+  }
+
+  useEffect(() => {
+    fetchHabits()
+  }, [])
+
+  if (loading) {
+    return (
+      <Loading />
+    )
+  }
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
@@ -34,14 +100,33 @@ export function Habit() {
           {dayAndMonth}
         </Text>
 
-        <ProgressBar progress={30}/>
+        <ProgressBar progress={habitProgress}/>
 
-        <View className='mt-6'>
-          <Checkbox 
-            title='Caminhar'
-            checked={true}
-          />
+        <View className={clsx('mt-6', {
+          ['opacity-50']: isDateInPast
+        })}>
+          {
+            dayInfo?.possibleHabits ?
+            dayInfo?.possibleHabits.map(habit => (
+              <Checkbox
+                key={habit.id}
+                title={habit.title}
+                checked={completedHabits.includes(habit.id)}
+                disabled={isDateInPast}
+                onPress={() => {handToggleHabit(habit.id)}}
+              />
+            ))
+            : <HabitsEmpty/>
+          }
         </View>
+
+        {
+          isDateInPast && (
+            <Text className='text-white mt-10 text-center'>
+              Você não pode editar hábitos de uma data passada.
+            </Text>
+          )
+        }
       </ScrollView>
     </View>
   )
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useCallback } from "react";
 import { View, Text, ScrollView, Alert } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import dayjs from "dayjs"
 
 import { api } from "../lib/axios";
@@ -41,11 +41,11 @@ export function Home() {
     }
   }
 
-  useEffect(() => {
+  useFocusEffect(useCallback(() => {
     fetchData()
-  }, [])
+  }, []))
 
-  if (!loading) {
+  if (loading) {
     return (
       <Loading />
     )
